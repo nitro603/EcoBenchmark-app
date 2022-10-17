45d9355d4cc1df73c187052a1720828a348ee661
nitro603 <tavaracristian93@gmail.com>
2022-10-17 19:37:11 -0400

    finished setting up window
    can now use functions with the application

--- window/main.py
+++ window/main.py
@@ -1,12 +1,44 @@
 import sys
-import os
-from PyQt.QtGui import QGuiApplication
-from PyQt6.QtQml import QQmlApplicationEngine
-from PyQt6.QtQuick import QQuickWindow
+from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QTextEdit, QVBoxLayout
+from PyQt6.QtGui import QIcon
+from random import randrange
 
-QQuickWindow.setSceneGraphBackend('software')
-app = QGuiApplication(sys.argv)
-engine = QQmlApplicationEngine()
-engine.quit.connect(app.quit)
-engine.load('./UI/main.qml')
-sys.exit(app.exec())
+class MyApp(QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle('EcoBenchmark')
+        self.setWindowIcon(QIcon('earth.ico'))
+        self.resize(600,350)
+        
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        
+        #widgets
+        button = QPushButton('&Begin Eco-analysis', clicked=self.calculateEcoScore)
+        self.output = QTextEdit()
+        
+        layout.addWidget(button)
+        layout.addWidget(self.output)
+    
+    def calculateEcoScore(self):
+        outputText = 'Computer Score is ' + str(randrange((100)))
+        self.output.setText(outputText)
+
+#only used when you run the application from a command prompt
+#later you'll have to change this
+app = QApplication(sys.argv)
+app.setStyleSheet('''
+    QWidget {
+        font-size: 25px;
+    }              
+    
+    QPushButton {
+        font-size: 20px
+    }
+                  
+                  ''')
+
+window = MyApp()
+window.show()
+
+app.exec()
