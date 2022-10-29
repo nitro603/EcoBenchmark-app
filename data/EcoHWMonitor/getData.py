2bed810b9ce1605d9bf5224fd77a5a34e8066585
Yahia Elhag <44278240+YahiaE@users.noreply.github.com>
2022-10-29 19:35:07 -0400

    Fix path issue in python data file

--- data/EcoHWMonitor/getData.py
+++ data/EcoHWMonitor/getData.py
@@ -1,5 +1,6 @@
 import subprocess
 import threading
+import os
 
 cpuTemp = 0;
 cpuUsage = 0;
@@ -9,11 +10,14 @@ gpuUsage = 0;
 gpuPowerDraw = 0;
 
 def getData():
+    path = os.path.abspath(os.getcwd())
+
     threading.Timer(5.0, getData).start()
-    cmdLine = "data\\EcoHWMonitor\\bin\\Debug\\EcoHWMonitor.exe"
+    cmdLine = path + "\\data\\EcoHWMonitor\\bin\\Debug\\EcoHWMonitor.exe"
+    print(cmdLine);
     process = subprocess.Popen(cmdLine, shell=True)
     process.communicate()
-    with open('data\\EcoHWMonitor\\stats\\data.txt') as d:
+    with open(path + '\\data\\EcoHWMonitor\\stats\\data.txt') as d:
          lines = d.readlines()
          if len(lines) > 0:
              cpuTemp, cpuUsage, cpuPowerDrawPackage, gpuTemp, gpuUsage, gpuPowerDraw = lines
