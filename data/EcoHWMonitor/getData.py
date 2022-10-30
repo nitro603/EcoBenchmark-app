6624893930c614cd624ae6e38c2cf4961a3ede62
nitro603 <tavaracristian93@gmail.com>
2022-10-29 21:21:07 -0400

    setup

--- data/EcoHWMonitor/getData.py
+++ data/EcoHWMonitor/getData.py
@@ -10,17 +10,17 @@ gpuUsage = 0;
 gpuPowerDraw = 0;
 
 def getData():
-    path = os.path.abspath(os.getcwd())
+    abspath = os.path.abspath(os.getcwd())
 
     threading.Timer(5.0, getData).start()
-    cmdLine = path + "\\data\\EcoHWMonitor\\bin\\Debug\\EcoHWMonitor.exe"
+    cmdLine = os.path.join(abspath, r"data\EcoHWMonitor\bin\Debug\EcoHWMonitor.exe")
     print(cmdLine);
-    process = subprocess.Popen(cmdLine, shell=True)
+    process = subprocess.Popen("\""+ r"D:\Desktop- SSD\COMP SCI\EXP Boost\Github\EcoBenchmark\EcoBenchmark\EcoBenchmark-app\data\EcoHWMonitor\bin\Debug\EcoHWMonitor.exe" + "\"", shell=True)
     process.communicate()
-    with open(path + '\\data\\EcoHWMonitor\\stats\\data.txt') as d:
-         lines = d.readlines()
-         if len(lines) > 0:
-             cpuTemp, cpuUsage, cpuPowerDrawPackage, gpuTemp, gpuUsage, gpuPowerDraw = lines
+    with open(r'D:\Desktop- SSD\COMP SCI\EXP Boost\Github\EcoBenchmark\EcoBenchmark\EcoBenchmark-app\data\EcoHWMonitor\stats\data.txt') as d:
+          lines = d.readlines()
+          if len(lines) > 0:
+              cpuTemp, cpuUsage, cpuPowerDrawPackage, gpuTemp, gpuUsage, gpuPowerDraw = lines
              
             
 
