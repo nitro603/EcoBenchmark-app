4424a9de5b66f14fe43e12326180669dce84c50e
nitro603 <tavaracristian93@gmail.com>
2022-10-30 22:01:22 -0400

    update

--- window/monitor.py
+++ window/monitor.py
@@ -14,7 +14,6 @@ def getData():
     abspath = os.path.abspath(os.getcwd())
 
     cmdLine = os.path.join(abspath, r"data\EcoHWMonitor\bin\Debug\EcoHWMonitor.exe")
-    print(cmdLine);
     process = subprocess.Popen("\""+ r"D:\Desktop- SSD\COMP SCI\EXP Boost\Github\EcoBenchmark\EcoBenchmark\EcoBenchmark-app\data\EcoHWMonitor\bin\Debug\EcoHWMonitor.exe" + "\"", shell=True)
     process.communicate()
     with open(r'D:\Desktop- SSD\COMP SCI\EXP Boost\Github\EcoBenchmark\EcoBenchmark\EcoBenchmark-app\data\EcoHWMonitor\stats\data.txt') as d:
